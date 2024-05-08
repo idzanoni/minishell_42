9397db42108217e918e9157db756c9988d2c998c
Izabel Zanoni <izanoni@c1r6p4.42sp.org.br>
2024-05-08 19:39:21 -0300

    .

---
+++ builtins.c
@@ -0,0 +1,8 @@
+char *bt_echo(char *splited_prompt);
+char *bt_echo_n(char *splited_prompt);
+char *bt_cd(char *splited_prompt);
+char *bt_pwd(v);
+char *bt_export(char *splited_prompt);
+char *bt_unset(char *splited_prompt);
+char *bt_env(char *splited_prompt);
+char *bt_exit(char *splited_prompt);
