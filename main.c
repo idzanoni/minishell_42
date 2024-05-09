72433f29f6bbfa175c688f76a62a10768d18038f
Izabel Zanoni <izanoni@c3r3p1.42sp.org.br>
2024-05-09 19:08:25 -0300

    .

--- main.c
+++ main.c
@@ -1,19 +1,33 @@
-#include "libft/libft.h"
-#include <readline/readline.h>
-#include <stdlib.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+
+struct sfd_in_out
+{
+	int fd_in;
+	int fd_out;
+}; 
+
+typedef struct sfd_in_out t_fds;;
+
 
 void	make_command(char *prompt, char **envp);
 char	*find_path(char *splited_prompt, char **envp);
 int		is_builtin(char *splited_prompt);
 char	*return_value(char **envp, char *var);
 int		input_check(char *prompt);
+char 	**remove_redirect(char **splited_prompt);
+void	mult_command(char **prompt_pipe, char **envp);
 
 void	minishell(char **envp)
 {
 	char	*prompt;
+	char	**prompt_pipe;
 	int		check;
+	
 
 	while (42)
 	{
@@ -26,15 +40,15 @@ void	minishell(char **envp)
 			if (check == 1)
 				break ;
 		}
-		// >arquivo
-		/* heredoc();
-		command_sep();
-		if (command_sep > 1)
+		// heredoc();
+		prompt_pipe = ft_split(prompt, '|');
+		// command_sep();
+		if (prompt_pipe[1])
 		{
-			mult_command();
-		} */
-		//else
-		make_command(prompt, envp);
+			mult_command(prompt_pipe, envp);
+		}		
+		else
+			make_command(prompt, envp);
 		printf("-------------\n");
 	}
 }
@@ -45,13 +59,14 @@ int	input_check(char *prompt)
 	//string vazia (só \0 e ctrl+d(nula)), aspas, dados completos << |
 	if (prompt == NULL)
 	{
-		return (1);
+		return(1);
 	}
 	if (prompt[0] == '\0')
 	{
-		return (2);
+		return(2);
 	}
-	return (0);
+
+	return(0);
 }
 
 /* void mult_command()
@@ -72,64 +87,128 @@ int	input_check(char *prompt)
 		command--;
 	}
 	wait_child();
+} */
+
+void	mult_command(char **prompt_pipe, char **envp)
+{
+	int	pipe_fds[2];
+	int	fork_mult;
+
+	pipe(pipe_fds);
+	fork_mult = fork();
+	if (fork_mult == 0)
+	{
+		dup2(pipe_fds[1], 1);
+		close(pipe_fds[0]);
+		close(pipe_fds[1]);
+		make_command(prompt_pipe[0], envp);
+		exit(0);
+	}
+	wait(NULL);
+	fork_mult = fork();
+	if (fork_mult == 0)
+	{
+		dup2(pipe_fds[0], 0);
+		close(pipe_fds[0]);
+		close(pipe_fds[1]);
+		make_command(prompt_pipe[1], envp);
+		exit(0);
+	}
+	wait(NULL);
+	close(pipe_fds[0]);
+	close(pipe_fds[1]);
 }
- */
+
 
 char	**remove_redirect(char **splited_prompt)
 {
-	int		count_line;
-	int		redirect_count;
+	int 	count_line;
+	int 	redirect_count;
 	char	**return_redirect;
-	int		copy_count;
 
 	count_line = 0;
 	redirect_count = 0;
-	while (splited_prompt[count_line] != NULL)
+
+	while(splited_prompt[count_line] != NULL)
 	{
-		if (splited_prompt[count_line][0] == '<'
-			|| splited_prompt[count_line][0] == '>')
-		{
-			copy_count = count_line;
-			while (splited_prompt[copy_count] != NULL)
-			{
-				splited_prompt[count_line] = splited_prompt[count_line + 1];
-				copy_count++;
-			}
-		}
-		count_line++;
+		if(splited_prompt[count_line][0] == '<' || splited_prompt[count_line][0] == '>')
+			redirect_count++;
+		count_line++;	
 	}
-	return_redirect = malloc((redirect_count + 1) * sizeof(char *));
+	return_redirect = malloc((redirect_count + 1 ) * sizeof(char*));
 	count_line = 0;
 	redirect_count = 0;
-	while (splited_prompt[count_line] != NULL)
+	while(splited_prompt[count_line] != NULL)
 	{
-		if (splited_prompt[count_line][0] == '<'
-			|| splited_prompt[count_line][0] == '>')
+		if(splited_prompt[count_line][0] == '<' || splited_prompt[count_line][0] == '>')
 		{
 			return_redirect[redirect_count] = ft_strdup(splited_prompt[count_line]);
 			redirect_count++;
 		}
-		count_line++;
+		count_line++;	
 	}
 	return_redirect[redirect_count] = NULL;
-	return (return_redirect);
+	count_line = 0;
+    while (splited_prompt[count_line] != NULL)
+    {
+        if(splited_prompt[count_line][0] == '<' || splited_prompt[count_line][0] == '>')
+        {
+            int copy_count = count_line;
+            while (splited_prompt[copy_count] != NULL)
+            {
+                splited_prompt[count_line] = splited_prompt[count_line + 1];
+                copy_count++;
+            }
+        }
+        count_line++;
+    }
+	return(return_redirect);
+}
+
+t_fds	check_redirect(char **redirect)
+{
+	int	redirect_count;
+	t_fds command_fd;
+
+	command_fd.fd_in = 0;
+	command_fd.fd_out = 1;
+	redirect_count = 0;
+	while (redirect[redirect_count] != NULL)
+	{
+		if (redirect[redirect_count][0] == '>') //out
+		{
+			if (command_fd.fd_out != 1)
+				close(command_fd.fd_out);
+			command_fd.fd_out = open(&redirect[redirect_count][1], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+		}
+		else if (redirect[redirect_count][0] == '<') //in
+		{
+			if (command_fd.fd_in != 0)
+				close(command_fd.fd_in);
+			command_fd.fd_in = open(&redirect[redirect_count][1], O_RDONLY);
+		}
+		redirect_count++;
+	}
+	return (command_fd);
 }
 
 void	make_command(char *prompt, char **envp)
 {
-	char	**splited_prompt;
+	char 	**splited_prompt;
 	char	*path_founded;
 	int		retorno_do_fork;
 	char	**redirect;
+	t_fds	command_fd;
 
 	splited_prompt = ft_split(prompt, ' ');
 	redirect = remove_redirect(splited_prompt);
-	for (int i = 0; redirect[i] != NULL; i++)
+	command_fd = check_redirect(redirect);
+/* 	for (int i = 0; redirect[i] != NULL; i++)
 		printf("%s\n", redirect[i]);
-	return ;
+	return; */
+
 	//remove_redirect(); // echo oi >arquivo <arquivo
 	//
-	//check_redirect();
 	//expand_remove_quote();
 	if (is_builtin(splited_prompt[0]) == 1)
 	{
@@ -139,19 +218,17 @@ void	make_command(char *prompt, char **envp)
 	else
 	{
 		path_founded = find_path(splited_prompt[0], envp);
-		// Find path retornou o caminho, agora é só mandar pro execve com
-		// O comando e o envp
-		// Vou jantar e já volto.
-		// Com isso já dá para executar 1 comando
-		// Se colocar o fork, vai dar pra executar vários commando
 		retorno_do_fork = fork();
 		if (retorno_do_fork == 0)
 		{
+			if (command_fd.fd_in != 0)
+				dup2(command_fd.fd_in, 0);
+			if (command_fd.fd_out != 1)
+				dup2(command_fd.fd_out, 1);
 			execve(path_founded, splited_prompt, envp);
 			exit(0);
 		}
 		wait(NULL);
-		printf("make: %s e %s\n", path_founded, splited_prompt[0]);
 	}
 }
 
@@ -165,15 +242,15 @@ char	*find_path(char *splited_prompt, char **envp)
 	i = 0;
 	path_env = return_value(envp, "PATH");
 	splited_path = ft_split(path_env, ':');
-	while (splited_path[i] != NULL)
+	while(splited_path[i] != NULL)
 	{
 		path = ft_strjoin(splited_path[i], "/");
 		path = ft_strjoin(path, splited_prompt);
 		if (access(path, F_OK) == 0)
-			return (path);
+			return(path);
 		i++;
 	}
-	return (NULL);
+	return(NULL);
 }
 
 int	is_builtin(char *splited_prompt)
@@ -187,8 +264,8 @@ int	is_builtin(char *splited_prompt)
 /* void freeeeee()
 {
 	free;
-} */
-
+}
+ */
 char	*return_value(char **envp, char *var)
 {
 	char	*value;
@@ -197,13 +274,13 @@ char	*return_value(char **envp, char *var)
 
 	i = 0;
 	len_var = ft_strlen(var);
-	while (envp[i] != NULL)
+	while(envp[i] != NULL)
 	{
-		value = ft_strnstr(envp[i], var, len_var);
-		if (value != NULL)
+		value = ft_strnstr (envp[i], var, len_var);
+		if(value != NULL)
 		{
 			if (envp[i][len_var] == '=')
-				return (&envp[i][len_var + 1]);
+				return(&envp[i][len_var + 1]);
 		}
 		i++;
 	}
@@ -213,5 +290,5 @@ char	*return_value(char **envp, char *var)
 int	main(int argc, char **argv, char **envp)
 {
 	minishell(envp);
-	return (0);
+	return(0);
 }
