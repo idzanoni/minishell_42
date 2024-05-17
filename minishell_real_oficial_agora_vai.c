53d17c2a7a01f44b6cfbf5763db5be8f9ada518f
Izabel Zanoni <izanoni@c3r2p1.42sp.org.br>
2024-05-17 19:31:31 -0300

    .

--- minishell_real_oficial_agora_vai.c
+++ minishell_real_oficial_agora_vai.c
@@ -6,12 +6,14 @@
 /*   By: izanoni <izanoni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 16:46:48 by izanoni           #+#    #+#             */
-/*   Updated: 2024/05/15 19:41:00 by izanoni          ###   ########.fr       */
+/*   Updated: 2024/05/17 19:28:45 by izanoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <readline/readline.h>
+#include <readline/history.h>
+
 
 /*
 #define DOUBLE_QUOTES '"'
@@ -59,8 +61,7 @@ void	minishell(char **envp)
 			if (check_prompt(prompt))
 			{
 				//string vazia ( só \0, aspas, dados completos > < >> <<  | )
-				check_quotes(prompt);
-
+			
 			}
 		}
 		printf("----\n");
@@ -69,92 +70,98 @@ void	minishell(char **envp)
 
 int	check_prompt(char *prompt)
 {
-	int		checker;
-	
-	//check_empty(prompt); // Comando está certo, não tem mensagem de erro
-	checker = check_quotes(prompt);
-	if (checker == -1)
-	{
-		printf("erro aspas\n"); // Se der ruim, tem que mostrar mensagem de erro
-		return(-1);
-	}
-	//check_redirect(prompt); // Se der ruim, tem que mostrar mensagem de erro
+	if (!check_empty(prompt)) // check_empty == 0 ?
+		return (-1); // Comando está certo, não tem mensagem de erro
+	add_history(prompt);
+	if (!check_quotes(prompt))
+		return (-1);
+	//check_redirect(prompt);
+	 // Se der ruim, tem que mostrar mensagem de erro
 	return(0);
 }
 int check_empty(char *prompt)
 {
 	if(prompt[0] == '\0')
+	{	
+		//printf("Error empty\n");
 		return(-1);
+	}
 	else
 		return(0);
 }
+
+int	only_space(char *prompt)
+{
+	int count;
+
+	count = 0;
+	while (prompt[count] != '\0')
+	{
+		if (prompt[count] != ' ' || prompt[count] != '\t') 
+			return(0);
+		count++;
+	}
+	return (-1);
+}  
+
 int check_redirect(char *prompt)
 {
-	// echo   oi				 < 										      file
-	// ls       				> file
-	int count;
+	int		count;
+	char	quote;
 
 	count = 0;
 	while(prompt[count] != '\0')
 	{
-		// ">            "
+		if (prompt[count] == 34 || prompt[count] == 39)
+		{
+			quote = prompt[count];
+			while (prompt[count] != quote)
+				count ++;
+		}
 		if(prompt[count] == '>' || prompt[count] == '<')
 		{
-			
+			count++;
+			if ((prompt[count] != prompt[count - 1]) && (prompt[count] == '>' || prompt[count] == '<'))
+				return(-1);
+			else if (prompt[count] == '>' || prompt[count] == '<')
+				count++;
+			while (prompt[count] == ' ' || prompt[count] == '\t')
+				count++;
+			if(prompt[count] == '\0' || prompt[count] == '|' 
+				|| prompt[count] == '>' || prompt[count] == '<')
+			{
+				printf("error redirect\n");
+				return(-1);
+			}
 		}
 		count++;
 	}
 	return(0);
 }
-/* int	check_quotes(char *prompt)
-{
-	int	count;
-	int count_quotes;
-
-	count = 0;
-	count_quotes = 0;
-	while(prompt[count] != '\0')
-	{
-		// echo oi "mensagem ' simples"
-		if(prompt[count] == '"' || prompt[count] == '\'')
-			count_quotes++;
-	}
-	if((count_quotes / 2) == 0)
-		return(0);
-	else
-		return(-1);	
-} */
 
 int	check_quotes(char *prompt)
 {
 	int i;
 	int quotes;
 	
-	i = 0;
-	while(prompt[i] != '\0')
+	quotes = 0;
+	i = -1;
+	while(prompt[++i] != '\0')
 	{
 		if (prompt[i] == 34 || prompt[i] == 39)
 		{
+			quotes = prompt[i];
 			if (prompt[i] == 34)
-			{
-				i++;
-				while (prompt[i] != 34 && prompt[i] != '\0')
-					i++;
-				if (prompt[i] != 34)
-					quotes = -1;
-			}
-			if (prompt[i] == 39)
-			{
-				i++;
-				while (prompt[i] != 39 && prompt[i] != '\0')
-					i++;
-				if (prompt[i] != 39)
-					quotes = -1;
-			}
+				while (prompt[++i] != 34 && prompt[i] != '\0')
+					;
+			else if (prompt[i] == 39)
+				while (prompt[++i] != 39 && prompt[i] != '\0')
+					;
+			if (prompt[i] != quotes)
+				quotes = -1;
 		}
 		if (prompt[i] == '\0')
 			break ;
-		i++;
 	}
 	return(quotes);
 }
