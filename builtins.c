54b690e38a1404c4833569e132a7f07fe0f06e15
Marcela Gonzaga <134085407+mgonzaga11@users.noreply.github.com>
2024-06-05 15:12:07 +0000

    echo

--- builtins.c
+++ builtins.c
@@ -1,5 +1,45 @@
-char	*bt_echo(char *splited_prompt, t_fds command_fd); //+n
+void	bt_echo(char *splited_prompt, t_fds command_fd)
+{
+    int count;
+    int n;
+    int val;
+
+    count = 1;
+    n = 0;
+    val = 0;
+    while(splited_prompt[count] != NULL)
+    {
+        if(splited_prompt[count][0] == '-')
+        {
+            if(splited_prompt[count][1] == 'n' && val == 0)
+            {    
+                count++;
+                n++;
+            }
+            else
+            {
+                ft_putstr_fd(splited_prompt[count], command_fd)
+                count++;   
+                val++;
+            }
+        }
+        else
+        {
+            ft_putstr_fd(splited_prompt[count], command_fd)
+            count++;
+			val++;  
+        }
+    }
+    if(n == 0)
+        write(1, '\n', 1);
+}
+
+
 char	*bt_cd(char *splited_prompt, t_fds command_fd);
+{
+
+}
+
 char	*bt_pwd(char *splited_prompt, t_fds command_fd);
 char	*bt_export(char *splited_prompt, t_fds command_fd);
 char	*bt_unset(char *splited_prompt, t_fds command_fd);
--- funcitions.c
+++ funcitions.c
@@ -144,3 +144,16 @@ char	*ft_strdup(const char *s)
 	copy[i] = '\0';
 	return (copy);
 }
+
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
