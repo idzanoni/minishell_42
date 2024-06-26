5dc2684494a2a8da9930d30e937754a400063524
Marcela Gonzaga <mgonzaga@c1r6p3.42sp.org.br>
2024-06-26 15:08:31 -0300

    atualizando

--- heredoc.c
+++ heredoc.c
@@ -6,7 +6,7 @@
 /*   By: mgonzaga <mgonzaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 19:27:02 by mgonzaga          #+#    #+#             */
-/*   Updated: 2024/06/24 18:17:07 by mgonzaga         ###   ########.fr       */
+/*   Updated: 2024/06/25 19:15:40 by mgonzaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,30 @@ void	heredoc(char	**prompt)
 	char	*limit;
 	int		count;
 	char	*cmp;
+	int		fd;
 	
 	count = 0;
-	while(prompt[count] != NULL)
+	
+	while (prompt[count] != NULL)
 	{
-		if(prompt[count][0] == '<' && prompt[count][1] == '<')
+		if (prompt[count][0] == '<' && prompt[count][1] == '<')
 		{
 			if(prompt[count] == NULL)
 				break;
 			count++;
 			limit = prompt[count];
-			open("arquivo", O_WRONLY | O_CREAT, 0644);
+			fd = open("arquivo", O_RDWR | O_CREAT, 0644);
 			cmp = readline(">");
-			while(ft_memcmp(limit, cmp, ft_strlen(limit)) != 0)
+			while (ft_memcmp(limit, cmp, ft_strlen(limit)) != 0)
 			{
 				cmp = readline(">");
+				write(fd, cmp, ft_strlen(cmp));
+				write(fd, "\n", 1);
 			}
-			printf("%s", prompt[count]);
 			count++;
 		}
 		else
 			count++;
-		if(prompt[count] == NULL)
-			break;
-		printf(" final do while %s\n", prompt[count]);
 	}
+	
 }
